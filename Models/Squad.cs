7084ee166449de9da616cd8cd751767c6c393762
KozlovOleg97 <kozlovoleg2021@gmail.com>
2022-11-17 22:42:03 +0300

    Add project files.

---
+++ Models/Squad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationParseFiles.Models
+{
+    [Serializable]
+    public class Squad
+    {
+        public string SquadName { get; set; }
+        public string Metro_City { get; set; }
+        public int Formed { get; set; }
+        public string SecretBase { get; set; }
+        public bool Active { get; set; }
+        public List<Members> MyProperty { get; set; }
+    }
+}
